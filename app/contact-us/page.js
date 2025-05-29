822a8780a491705181ea29abe810f98bf02065a5
veerrai <karamveer20000625@gmail.com>
2025-05-29 13:15:22 +0530

    conatct page add title

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -2,8 +2,19 @@ import Banner from '@/components/common/Banner'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
+import { metadata } from '../layout'
 
 const page = () => {
+    const generateMetaData = () => {
+    const metadata = {
+        title: "Contact Us | flightsclicks.com",
+        description: ""
+    };
+    return metadata;
+};
+
+const meta = generateMetaData();
+
     return (
         <>
         <Header />
